c179a70be804898e7d47e476896329dc2c072854
kookh <kookh>
2023-07-10 22:51:37 +0900

    change : port 3000 into 80

--- app/routes/route.js
+++ app/routes/route.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", (req, res) => {
-  return res.status(200).send(process.env.NODE_ENV + " : working! 2023-07-05");
+  return res.status(200).send("working! 2023-07-10");
 });
 
 module.exports = router;
