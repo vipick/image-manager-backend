8075cff1ac66d6cdaad153311da0ebd221c91689
kookh <kookh>
2023-07-23 16:15:46 +0900

    fix : image upload

--- app/commons/image-upload-ncloud.js
+++ app/commons/image-upload-ncloud.js
@@ -8,9 +8,6 @@ const region = "kr-standard";
 const accessKey = process.env.PROD_NCP_ACCESS_KEY;
 const secretKey = process.env.PROD_NCP_SECRET_KEY;
 const imageBucket = process.env.PROD_NCP_IMAGE_BUCKET;
-const fileType = "image/jpg";
-const fileName = uuid.v4();
-const fileSize = 10 * 1024 * 1024;
 
 const S3 = new AWS.S3({
   endpoint,
@@ -27,17 +24,17 @@ const upload = multer({
     bucket: imageBucket,
     acl: "public-read",
     contentType: function (req, file, cb) {
-      cb(null, fileType);
+      cb(null, "image/jpg");
     },
     key(req, file, cb) {
       try {
-        cb(null, `${fileName}`);
+        cb(null, `${uuid.v4()}`);
       } catch (error) {
         console.log(error);
       }
     },
   }),
-  limits: { fileSize: fileSize },
+  limits: { fileSize: 10 * 1024 * 1024 },
 });
 
 module.exports = upload;
